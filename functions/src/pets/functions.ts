980e93e684ea692b0f9e87b511ca81fe08ee52cb
Tuan Nguyen <tuan.nguyen@kanthorlabs.com>
2024-10-13 19:32:16 +0700

    chore: update schedule

--- functions/src/pets/functions.ts
+++ functions/src/pets/functions.ts
@@ -1,8 +1,33 @@
 import { onSchedule } from "firebase-functions/v2/scheduler";
+import admin from "firebase-admin";
+import { ulid } from "ulid";
+import fetch from "node-fetch";
 
 import * as settings from "../settings";
 
-export const firstpet = onSchedule(
-  { region: settings.FIREBASE_REGION, schedule: "every 10 minutes" },
-  async () => {}
+export const petno_1 = onSchedule(
+  { region: settings.FIREBASE_REGION, schedule: "every 3 minutes" },
+  async () => {
+    const ip = await fetch("https://api.ipify.org/").then((r) => r.text());
+    const id = ulid();
+    await admin.firestore().collection("ip").doc(id).set({ ip, id });
+  }
+);
+
+export const petno_2 = onSchedule(
+  { region: settings.FIREBASE_REGION, schedule: "every 5 minutes" },
+  async () => {
+    const ip = await fetch("https://api.ipify.org/").then((r) => r.text());
+    const id = ulid();
+    await admin.firestore().collection("ip").doc(id).set({ ip, id });
+  }
+);
+
+export const petno_3 = onSchedule(
+  { region: settings.FIREBASE_REGION, schedule: "every 7 minutes" },
+  async () => {
+    const ip = await fetch("https://api.ipify.org/").then((r) => r.text());
+    const id = ulid();
+    await admin.firestore().collection("ip").doc(id).set({ ip, id });
+  }
 );
